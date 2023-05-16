97398ba0a442bc1dbb10fedc342362a05fa81716
2jcortesj2 <108505600+2jcortesj2@users.noreply.github.com>
2023-05-15 22:29:15 -0500

    Update

--- Pruebas.m
+++ Pruebas.m
@@ -1,12 +1,25 @@
-syms x y; % Variables simbólicas
+% Cadena de texto original
+str = 'real(x^2*y*3i) - 3*real(x*y^2) + imag(y^3) + real(x^3)';
 
-% Define la función compleja f(x, y)
-f = x^2 + 2*x*y - y^2 + 3i*x - 4i*y;
+% Expresión regular para extraer las partes dentro de "real()" con signos y multiplicadores
+pattern = '([^+^-]*)real\((.*?)\)';
 
-% Halla las partes reales e imaginarias de la funcion
-z = symfun(eval(f), x);
-u = real(z);
-v = imag(z);
-function_parts = [u, v];
-char(u)
-disp(v)
+% Buscar todas las coincidencias en la cadena de texto
+matches = regexp(str, pattern, 'tokens');
+
+% Concatenar las partes encontradas con signos y multiplicadores
+result = '';
+for i = 1:length(matches)
+    match = matches{i};
+    if numel(match) == 2
+        pre_text = match{1};
+        inner_text = match{2};
+        result = [result pre_text 'real(' inner_text ') '];
+    end
+end
+
+% Eliminar los espacios en blanco adicionales al inicio y al final
+result = strtrim(result);
+
+% Mostrar el resultado
+disp(result);
